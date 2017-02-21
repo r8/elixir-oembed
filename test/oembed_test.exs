29c49c4e57d4c7444c1b41f1f8923f96e20f624c
Sergey Storchay <r8@r8.com.ua>
2017-02-21 19:40:17 +0200

    Update readme

--- test/oembed_test.exs
+++ test/oembed_test.exs
@@ -11,5 +11,8 @@ defmodule OEmbedTest do
     IO.inspect OEmbed.for("http://www.youtube.com/watch?v=K4aeibd1Rrc")
     IO.inspect OEmbed.for("https://www.instagram.com/p/BQwNhqoADJn/")
     IO.inspect OEmbed.for("http://www.instagram.com/p/BQwNhqoADJn/")
+    IO.inspect OEmbed.for("http://www.instagram.com/p/BQ/")
+    IO.inspect OEmbed.for("https://www.youtube.com/watch?v=K4a")
+    IO.inspect OEmbed.for("https://www.google.com")
   end
 end
