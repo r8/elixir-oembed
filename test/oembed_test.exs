1c353e3d253660a50fe38c7ef76c6a02c6e07cd9
Sergey Storchay <r8@r8.com.ua>
2017-02-25 19:28:38 +0200

    Add pinterest tests

--- test/oembed_test.exs
+++ test/oembed_test.exs
@@ -37,4 +37,17 @@ defmodule OEmbedTest do
       {:error, _} = OEmbed.for("https://www.instagram.com/p/invalid_url/")
     end
   end
+
+  test "gets rich oembed for valid pinterest pin url" do
+    use_cassette "pinterest_pin_valid" do
+      {:ok, %Rich{} = oembed} = OEmbed.for("https://www.pinterest.com/pin/731483164446834719/")
+      assert oembed.html =~ "embedPin"
+    end
+  end
+
+  test "gets error respone for invalid pinterest url" do
+    use_cassette "pinterest_pin_invalid" do
+      {:error, _} = OEmbed.for("https://www.pinterest.com/pin/invalid_url/")
+    end
+  end
 end
