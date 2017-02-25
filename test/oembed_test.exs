60819daa86c2c9be91161b3f4fc9049a1e637189
Sergey Storchay <r8@r8.com.ua>
2017-02-25 19:22:23 +0200

    Add instagram tests

--- test/oembed_test.exs
+++ test/oembed_test.exs
@@ -3,6 +3,7 @@ defmodule OEmbedTest do
   use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
   alias OEmbed.Video
+  alias OEmbed.Rich
 
   test "gets video oembed for valid youtube url" do
     use_cassette "youtube_valid" do
@@ -14,6 +15,26 @@ defmodule OEmbedTest do
   test "gets error respone for invalid youtube url" do
     use_cassette "youtube_invalid" do
       {:error, _} = OEmbed.for("https://www.youtube.com/watch?v=invalid_url")
+    end
+  end
+
+  test "gets rich oembed for valid instagram url" do
+    use_cassette "instagram_valid" do
+      {:ok, %Rich{} = oembed} = OEmbed.for("https://www.instagram.com/p/7OZIdGDvFT/")
+      assert oembed.html =~ "instagram-media"
+    end
+  end
+
+  test "gets rich oembed for valid instagram short url" do
+    use_cassette "instagram_short_valid" do
+      {:ok, %Rich{} = oembed} = OEmbed.for("http://instagr.am/p/7OZIdGDvFT/")
+      assert oembed.html =~ "instagram-media"
+    end
+  end
+
+  test "gets error respone for invalid instagram url" do
+    use_cassette "instagram_invalid" do
+      {:error, _} = OEmbed.for("https://www.instagram.com/p/invalid_url/")
     end
   end
 end
