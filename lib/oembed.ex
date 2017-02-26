098b2b79a32a442a672c22b002f393bf88f6d71b
Sergey Storchay <r8@r8.com.ua>
2017-02-26 12:04:00 +0200

    Update module doc

--- lib/oembed.ex
+++ lib/oembed.ex
@@ -2,6 +2,26 @@ defmodule OEmbed do
   @moduledoc """
   oEmbed consumer library for Elixir applications.
 
+  > oEmbed is a format for allowing an embedded representation of a URL on third party sites. The simple API allows a website to display embedded content (such as photos or videos) when a user posts a link to that resource, without having to parse the resource directly.
+  >
+  > -- See [oembed.com](http://oembed.com) for more info about the protocol.
+
+  This library supports any discoverable oEmbed endpoint and some other services via custom adapters.
+  Among them:
+
+    - Instagram
+    - Pinterest
+
+  ## Installation
+
+  Add `oembed` to your list of dependencies in `mix.exs`:
+
+  ```elixir
+  def deps do
+    [{:oembed, "~> 0.1.0"}]
+  end
+  ```
+
   ## Usage
 
   ```elixir
