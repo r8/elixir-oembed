5e5ddbf364e54ca481fac36f622fbcf0036f591a
Vyacheslav Savelyev <v.savelyev@rambler-co.ru>
2017-10-01 21:02:04 +0300

    fix crashing with :badarg on a discovereable relative protocol url

--- lib/oembed/providers/discoverable_provider.ex
+++ lib/oembed/providers/discoverable_provider.ex
@@ -27,10 +27,11 @@ defmodule OEmbed.DiscoverableProvider do
 
   defp discover(url) do
     with {:ok, %HTTPoison.Response{body: html}} <- HTTPoison.get(url, [], [follow_redirect: true, ssl: [{:versions, [:'tlsv1.2']}]]),
-      [_ | _] = tags <- Floki.find(html, "head link[type='application/json+oembed']"),
-       {"link", attributes, _} <- List.first(tags),
-       %{"href" => href} <- Enum.into(attributes, %{}) do
-        {:ok, href}
+         [_ | _] = tags <- Floki.find(html, "head link[type='application/json+oembed']"),
+         {"link", attributes, _} <- List.first(tags),
+         %{"href" => href} <- Enum.into(attributes, %{})
+    do
+      {:ok, String.replace(href, ~r{^//}, "http://")}
     else
       _ -> {:error, "oEmbed url not found"}
     end
