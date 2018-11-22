7e8c59ed0012d129eb70a0ba99b75f5dbb72eeb3
Sergey Storchay <r8@r8.com.ua>
2018-11-22 22:38:12 +0200

    Apply elixir format to all files

--- lib/oembed/providers/discoverable_provider.ex
+++ lib/oembed/providers/discoverable_provider.ex
@@ -17,8 +17,8 @@ defmodule OEmbed.DiscoverableProvider do
   """
   def get(url) do
     with {:ok, href} <- discover(url),
-      {:ok, oembed} <- get_oembed(href) do
-        {:ok, oembed}
+         {:ok, oembed} <- get_oembed(href) do
+      {:ok, oembed}
     else
       _ ->
         {:error, "oEmbed not found"}
@@ -26,12 +26,12 @@ defmodule OEmbed.DiscoverableProvider do
   end
 
   defp discover(url) do
-    with {:ok, %HTTPoison.Response{body: html}} <- HTTPoison.get(url, [], [follow_redirect: true, ssl: [{:versions, [:'tlsv1.2']}]]),
+    with {:ok, %HTTPoison.Response{body: html}} <-
+           HTTPoison.get(url, [], follow_redirect: true, ssl: [{:versions, [:"tlsv1.2"]}]),
          [_ | _] = tags <- Floki.find(html, "head link[type$='json+oembed']"),
          {"link", attributes, _} <- List.first(tags),
          %{"href" => href} <- Enum.into(attributes, %{}),
-         oembed_url = %URI{} <- URI.merge(url, href)
-    do
+         oembed_url = %URI{} <- URI.merge(url, href) do
       {:ok, URI.to_string(oembed_url)}
     else
       _ -> {:error, "oEmbed url not found"}
--- lib/oembed/resource.ex
+++ lib/oembed/resource.ex
@@ -7,11 +7,19 @@ defmodule OEmbed.Resource do
     quote do
       @derive [Poison.Encoder]
 
-      @common_keys [type: nil, version: "1.0", title: nil,
-                      author_name: nil, author_url: nil,
-                      provider_name: nil, provider_url: nil, cache_age: nil,
-                      thumbnail_url: nil, thumbnail_width: nil,
-                      thumbnail_height: nil]
+      @common_keys [
+        type: nil,
+        version: "1.0",
+        title: nil,
+        author_name: nil,
+        author_url: nil,
+        provider_name: nil,
+        provider_url: nil,
+        cache_age: nil,
+        thumbnail_url: nil,
+        thumbnail_width: nil,
+        thumbnail_height: nil
+      ]
 
       defstruct @common_keys ++ @keys
 
