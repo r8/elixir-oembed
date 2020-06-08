58c5d56750dc06c921116ff6860af8660e9efd4e
Sergey Storchay <r8@r8.com.ua>
2020-06-08 22:40:48 +0300

    Implicitly exclude "deps/credo/.credo.exs" from credo checks

--- .credo.exs
+++ .credo.exs
@@ -31,7 +31,7 @@
           "apps/*/test/",
           "apps/*/web/"
         ],
-        excluded: [~r"/_build/", ~r"/deps/", ~r"/node_modules/"]
+        excluded: [~r"/_build/", ~r"/deps/", ~r"/node_modules/", "deps/credo/.credo.exs"]
       },
       #
       # Load and configure plugins here:
