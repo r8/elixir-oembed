b6c7cd6d21541c2a940e477b6f07618eb99378fa
Alex Gleason <alex@alexgleason.me>
2021-04-29 15:14:22 -0500

    Remove unused @requred_keys

--- lib/oembed/resources/link.ex
+++ lib/oembed/resources/link.ex
@@ -5,7 +5,5 @@ defmodule OEmbed.Link do
 
   @keys [type: "link"]
 
-  @requred_keys []
-
   use OEmbed.Resource
 end
--- lib/oembed/resources/photo.ex
+++ lib/oembed/resources/photo.ex
@@ -5,7 +5,5 @@ defmodule OEmbed.Photo do
 
   @keys [type: "photo", url: nil, width: nil, height: nil]
 
-  @requred_keys [:url, :width, :height]
-
   use OEmbed.Resource
 end
